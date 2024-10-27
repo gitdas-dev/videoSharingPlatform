884fcd6e4314468a878b30ab7cc01f0dbfbf7a48
gitdas-dev <50999577+gitdas-dev@users.noreply.github.com>
2024-10-27 21:48:47 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,16 +2,15 @@ import axios from "axios"
 
 
 export const api = axios.create({
-    baseURL: "https://video-sharing-platformspi2.vercel.app",
-    withCredentials: true
+    baseURL: "https://video-sharing-platformspi2.vercel.app"
 });
-const token = JSON.parse(sessionStorage.getItem('token'));
 
 export const apiWithAuth = axios.create({
     baseURL: "https://video-sharing-platformspi2.vercel.app",
+    withCredentials: true,
     headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${JSON.parse(sessionStorage.getItem('token'))}`,
     }
-
+    
 })
 
