9ccd51b6429f9d93b26494a7e8310e11fcee7feb
gitdas-dev <50999577+gitdas-dev@users.noreply.github.com>
2024-10-28 13:07:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,9 +10,8 @@ import cors from "cors";
 const app = express();
 
 const corsOptions = {
-  origin: 'https://video-sharing-platform-frontend-seven.vercel.app',
+  origin: ['https://video-sharing-platform-frontend-seven.vercel.app'],
   methods: 'GET,POST,PUT,DELETE,OPTIONS',
-  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
 
